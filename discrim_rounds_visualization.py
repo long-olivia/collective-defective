3281db15a6126d4de7fd16b306151d69081ea506
olivia-long127 <ol2256@columbia.edu>
2025-07-19 17:28:06 -0500

    change

--- discrim_rounds_visualization.py
+++ discrim_rounds_visualization.py
@@ -5,16 +5,16 @@ import json
 def plot(prompt_pair, arr, gpt_err, claude_err):
     gpt=arr[0]
     claude=arr[1]
-    width=0.3
+    width=0.35
     x = np.arange(20)
     x_labels=[str(i) for i in range(1, 21)]
-    plt.figure(figsize=(15, 7))
+    plt.figure(figsize=(40, 7))
     plt.title(f"Average Contribution per Round, {prompt_pair}, Name Condition")
     gpt_bars=plt.bar(np.arange(len(gpt)), gpt, width=width, yerr=gpt_err, capsize=5, color='powderblue', label='GPT-4o')
     claude_bars=plt.bar(np.arange(len(claude)) + width, claude, width=width, yerr= claude_err, capsize=5, color='teal', label='Sonnet 4')
     plt.bar_label(gpt_bars, fmt='%.1f', padding=5)
     plt.bar_label(claude_bars, fmt='%.1f', padding=5)
-    plt.xticks(x, x_labels)
+    plt.xticks(x+width/2, x_labels)
     plt.xlabel("Rounds")
     plt.ylabel("Points Contributed")
     plt.grid(color='#95a5a6', linestyle='--', linewidth=1, axis='y', alpha=0.7)
@@ -44,4 +44,4 @@ def prepare(prompt_pair):
     plot(labels[prompt_pair], arr, err[0], err[1])
     
 if __name__ == "__main__":
-    prepare("SS")
+    prepare("SC")
