07356b6de877b31b1d85e98c613a673fd4688544
Olivia <ol2256@columbia.edu>
2025-08-02 18:14:36 +0000

    study 3 running

---
+++ Study 3/four_run_basic.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+pairs=(
+  "collective collective collective collective"
+  "neutral neutral neutral neutral"
+  "selfish selfish selfish selfish"
+)
+
+for pair in "${pairs[@]}"
+do
+    for i in {1..50}
+        do
+            echo "Running with: $pair, round $i"
+            python four_rephrased_basic.py $pair
+            sleep 1
+        done
+done
+
+deactivate
