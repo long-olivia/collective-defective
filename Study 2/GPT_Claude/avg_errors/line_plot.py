df5b4ca87026d9040ba0d9eb6dafa69516462255
olivia-long127 <ol2256@columbia.edu>
2025-08-21 00:13:50 -0500

    files

--- Study 2/GPT_Claude/avg_errors/line_plot.py
+++ Study 2/GPT_Claude/avg_errors/line_plot.py
@@ -72,10 +72,10 @@ df_all = pd.DataFrame(all_data)
 markers = {'Name': 'D', 'No-Name': 'o'}
 colors = {'GPT-4o': 'teal', 'Sonnet 4': 'lightcoral'}
 
-fig, axes = plt.subplots(3, 3, figsize=(20, 12), sharex=True, sharey=True)
+fig, axes = plt.subplots(2, 3, figsize=(16, 9), sharex=True, sharey=True)
 axes = axes.flatten()
 sns.set_theme(style='white')
-pairings_to_plot = ["CC", "CN", "CS", "NC", "NN", "NS", "SC", "SN", "SS"]
+pairings_to_plot = ["CS", "NC", "NN", "NS", "SC", "SS"]
 df_filtered=df_all[df_all['Prompt_Pairing'].isin(pairings_to_plot)]
 titles = {
     "CC": "Collective - GPT-4o, Collective - Sonnet 4",
@@ -134,4 +134,4 @@ for i, pairing in enumerate(pairings_to_plot):
 plt.tight_layout()
 # plt.subplots_adjust(top=0.94)
 # plt.suptitle('Study 2: Per Round Model Contributions by Prompt Pairing (GPT-4o - Sonnet 4)')
-plt.savefig('study2_gc', dpi=600)
+plt.savefig('study2_gc_short', dpi=600)
