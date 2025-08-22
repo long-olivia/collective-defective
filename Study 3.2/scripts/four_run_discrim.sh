c9989e494c25633aa6da282542630a7b9a092148
Olivia <ol2256@columbia.edu>
2025-08-22 15:53:06 +0000

    files

--- Study 3.2/scripts/four_run_discrim.sh
+++ Study 3.2/scripts/four_run_discrim.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 pairs=(
-  # "collective collective collective collective"
+  "collective collective collective collective"
   # "neutral neutral neutral neutral"
-  "self self self self"
+  # "self self self self"
 )
 
 models=(
   # "openai/gpt-4o"
   # "anthropic/claude-sonnet-4"
-  "meta-llama/llama-4-maverick"
-  # "qwen/qwen3-235b-a22b-2507"
+  # "meta-llama/llama-4-maverick"
+  "qwen/qwen3-235b-a22b-2507"
 )
 
 for model in "${models[@]}"
